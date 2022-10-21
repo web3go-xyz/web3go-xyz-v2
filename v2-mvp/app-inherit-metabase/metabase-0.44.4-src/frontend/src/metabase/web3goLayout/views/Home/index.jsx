dbfe4a8b30b6e80c3f686f052a303d6ee4eebc57
zhangzhi <zhanggoodzhi@web3go.xyz>
2022-10-21 18:36:33 +0800

    metamask login

--- v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/views/Home/index.jsx
+++ v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/views/Home/index.jsx
@@ -49,9 +49,56 @@ class Component extends React.Component {
                     <div className="section-title">
                         <span>My Space</span>
                         <svg className="arrow" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.24269 3.75667L13.4854 7.99933L9.24269 12.242M1.66669 8H13.0984" stroke-width="1.5" />
+                            <path d="M9.24269 3.75667L13.4854 7.99933L9.24269 12.242M1.66669 8H13.0984" strokeWidth="1.5" />
                         </svg>
-
+                    </div>
+                    <div className="section-content info">
+                        <div className="info-item hover-item">
+                            <div className="i-left">
+                                <div className="circle">
+                                    <img src={require("@/web3goLayout/assets/home/info1.png")} alt="" />
+                                </div>
+                                <div className="text-wrap">
+                                    <div className="label">Dashboards</div>
+                                    <div className="value">800,246</div>
+                                </div>
+                            </div>
+                            <img src={require("@/web3goLayout/assets/home/info11.png")} alt="" />
+                        </div>
+                        <div className="info-item hover-item">
+                            <div className="i-left">
+                                <div className="circle">
+                                    <img src={require("@/web3goLayout/assets/home/info2.png")} alt="" />
+                                </div>
+                                <div className="text-wrap">
+                                    <div className="label">Datasets</div>
+                                    <div className="value">800,246</div>
+                                </div>
+                            </div>
+                            <img src={require("@/web3goLayout/assets/home/info22.png")} alt="" />
+                        </div>
+                        <div className="info-item hover-item">
+                            <div className="i-left">
+                                <div className="circle">
+                                    <img src={require("@/web3goLayout/assets/home/info3.png")} alt="" />
+                                </div>
+                                <div className="text-wrap">
+                                    <div className="label">My Favorites</div>
+                                    <div className="value">800,246</div>
+                                </div>
+                            </div>
+                            <img src={require("@/web3goLayout/assets/home/info33.png")} alt="" />
+                        </div>
+                        <div className="btn hover-item">
+                            <img src={require("@/web3goLayout/assets/home/add.png")} alt="" />
+                            <span>New Dashboard</span>
+                        </div>
+                    </div>
+                    <div className="section-title">
+                        <span>Dashboard</span>
+                        <svg className="arrow" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.24269 3.75667L13.4854 7.99933L9.24269 12.242M1.66669 8H13.0984" strokeWidth="1.5" />
+                        </svg>
                     </div>
                 </div>
             </div >
