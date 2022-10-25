46e3d7a37014cd31b1b97c6133aa165f701cadee
zhangzhi <zhanggoodzhi@web3go.xyz>
2022-10-25 16:20:16 +0800

    homepage style

--- v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/views/Home/index.jsx
+++ v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/views/Home/index.jsx
@@ -7,6 +7,8 @@ import { push } from "react-router-redux";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
 import event from '@/web3goLayout/event';
+import { IconPlus, IconCheck } from '@arco-design/web-react/icon';
+import { numberSplit } from '@/web3goLayout/utils';
 import DashBoardList from './DashBoardList'
 const mapStateToProps = state => {
     return {
@@ -28,7 +30,8 @@ class Component extends React.Component {
                 require("@/web3goLayout/assets/home/banner.png"),
                 require("@/web3goLayout/assets/home/banner.png"),
                 require("@/web3goLayout/assets/home/banner.png"),
-            ]
+            ],
+            creatorList: [{}, {}, {}, {}, {}, {}]
         }
     }
     goSignIn = () => {
@@ -105,14 +108,13 @@ class Component extends React.Component {
                                     </div>
                                     <img src={require("@/web3goLayout/assets/home/info33.png")} alt="" />
                                 </div>
-                                <div className="btn hover-item">
+                                <div className="btn hover-item" onClick={() => { this.props.push('/home'); }}>
                                     <img src={require("@/web3goLayout/assets/home/add.png")} alt="" />
                                     <span>New Dashboard</span>
                                 </div>
                             </div >
                         )
                     }
-
                     <div className="section-title">
                         <span>Dashboard</span>
                         <svg className="arrow" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -121,6 +123,40 @@ class Component extends React.Component {
                     </div>
                     <div className="section-content">
                         <DashBoardList></DashBoardList>
+                    </div>
+                    <div className="section-title">
+                        <span>Top Creators</span>
+                        <svg className="arrow" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.24269 3.75667L13.4854 7.99933L9.24269 12.242M1.66669 8H13.0984" strokeWidth="1.5" />
+                        </svg>
+                    </div>
+                    <div className="section-content creator-list">
+                        {this.state.creatorList.map((v, i) => (
+                            <div key={i} className={"item" + (i == 0 ? ' active' : '')}>
+                                <div className="i-top">
+                                    <div className="headicon-wrap">
+                                        <img className="icon" src={require(`@/web3goLayout/assets/home/${i + 1 < 5 ? i + 1 : 4}.png`)} alt="" />
+                                        <span>{i + 1}</span>
+                                        <img className="headicon" src={require("@/web3goLayout/assets/account/Avatar.png")} alt="" />
+                                    </div>
+                                    <div className="it-right">
+                                        <div className="name">Cameron Williamson</div>
+                                        {/* <div className="btn">
+                                            <IconCheck />
+                                            <span className="text">Following</span>
+                                        </div> */}
+                                        <div className="btn hover-item">
+                                            <IconPlus />
+                                            <span className="text">Follow</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="i-bottom">
+                                    <span className="label">Dashboards</span>
+                                    <span className="value">{numberSplit(85948)}</span>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div >
             </div >
