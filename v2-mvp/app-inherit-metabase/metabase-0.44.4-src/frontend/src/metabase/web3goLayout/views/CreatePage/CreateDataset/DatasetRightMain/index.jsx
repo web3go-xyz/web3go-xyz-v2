d103a4c1ca5a1e9841dd34cde8fa8d377886fbaf
zhangzhi <4742944894@qq.com>
2023-02-22 17:09:00 +0800

    dataset

---
+++ v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/views/CreatePage/CreateDataset/DatasetRightMain/index.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { connect } from "react-redux";
+import './index.less';
+import { Button, Modal, Form, Input, Upload, Message, AutoComplete, Tabs, Typography, Tooltip, Select, Spin, Switch, Collapse } from '@arco-design/web-react';
+import { push, replace } from "react-router-redux";
+import QueryBuilder from "metabase/query_builder/containers/QueryBuilder";
+import {
+    getURLForCardState,
+} from "@/query_builder/utils";
+import { parse as parseUrl } from "url";
+
+const CollapseItem = Collapse.Item;
+const Option = Select.Option;
+const mapStateToProps = state => {
+    return {
+        currentUser: state.currentUser,
+        isDark: state.app.isDark,
+        userData: state.app.userData,
+        databaseList: state.entities.databases,
+    }
+};
+const mapDispatchToProps = {
+    push,
+    replace
+};
+const FormItem = Form.Item;
+
+class Component extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            refreshFlag: true,
+        }
+    }
+    componentDidMount() {
+        this.props.onRef(this);
+    }
+    init(v) {
+        const newState = {
+            "card": {
+                "dataset_query": {
+                    "database": v.db_id,
+                    "type": "query",
+                    "query": {
+                        "source-table": v.id
+                    }
+                },
+                "visualization_settings": {},
+                "display": "table"
+            }
+        };
+        const url = getURLForCardState(newState, true, {}, undefined);
+        const urlParsed = parseUrl(url);
+        const locationDescriptor = {
+            pathname: window.location.pathname,
+            search: urlParsed.search,
+            // hash: encodeURIComponent(urlParsed.hash),
+            hash: urlParsed.hash,
+            state: newState
+        };
+        this.props.replace(locationDescriptor);
+        this.setState({
+            refreshFlag: false
+        }, () => {
+            this.setState({
+                refreshFlag: true
+            })
+        });
+    }
+    render() {
+        const { refreshFlag } = this.state;
+        return (
+            <div className="web3go-dataset-create-right-main">
+                {
+                    refreshFlag ? <QueryBuilder notebook={true} {...this.props}></QueryBuilder> : null
+                }
+            </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Component);
