c5c24012ff0ba169d6fed3d6cefc58207df84344
Pan <imqipan@gmail.com>
2023-03-15 18:39:56 +0800

    optimizing the listCreator api, introduced new props

--- v2-mvp/api-service-extend/src/creator/model/QueryTopCreatorRequest.ts
+++ v2-mvp/api-service-extend/src/creator/model/QueryTopCreatorRequest.ts
@@ -5,4 +5,6 @@ import { PageRequest } from "src/viewModel/base/pageRequest";
 
 export class QueryTopCreatorRequest extends PageRequest {
 
+    @ApiProperty({ description: 'any texts that possibly matched by the accountName', default: null })
+    accountName: string;
 }
