3b3b5afa4b3c58b04e72500995ccad6b4dc6b53e
Pan <imqipan@gmail.com>
2023-03-15 16:38:29 +0800

    optimize the listCreator api, added count data for dataset

--- v2-mvp/api-service-extend/src/creator/creator.service.ts
+++ v2-mvp/api-service-extend/src/creator/creator.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DashboardExt } from 'src/base/entity/platform-dashboard/DashboardExt';
+import { DatasetExt } from 'src/base/entity/platform-dataset/DatasetExt';
 import { Account } from 'src/base/entity/platform-user/Account.entity';
 import { W3Logger } from 'src/base/log/logger.service';
 import { RepositoryConsts } from 'src/base/orm/repositoryConsts';
@@ -14,7 +15,10 @@ export class CreatorService {
     logger: W3Logger;
     constructor(
         @Inject(RepositoryConsts.REPOSITORYS_PLATFORM.PLATFORM_DASHBOARD_EXT_REPOSITORY.provide)
-        private dextRepo: Repository<DashboardExt>,) {
+        private dextRepo: Repository<DashboardExt>,
+        @Inject(RepositoryConsts.REPOSITORYS_PLATFORM.PLATFORM_DATASET_EXT_REPOSITORY.provide)
+        private datasetExtRepo: Repository<DatasetExt>,
+        ) {
         this.logger = new W3Logger(`CreatorService`);
     }
 
@@ -55,6 +59,8 @@ export class CreatorService {
 
         let records = await query.getRawMany();
 
+        const datasetStatByAccounts = await this.getDatasetStatByAccounts(records.map(it => it.creator_account_id));
+
         for (const t of records) {
             resp.list.push({
                 creator_account_id: t.creator_account_id,
@@ -65,10 +71,42 @@ export class CreatorService {
                 total_fork_count: t.total_fork_count,
                 total_favorite_count: t.total_favorite_count,
                 followed_account_count: t.followed_account_count,
+                dataset: datasetStatByAccounts[t.creator_account_id] || {}
             })
         }
-
         return resp;
     }
 
+
+    private async getDatasetStatByAccounts(accountIds) {
+        if (!accountIds || !accountIds.length) {
+            return {};
+        }
+        const query = this.datasetExtRepo
+          .createQueryBuilder('d')
+          .where('d.creator_account_id IN( :...creator_account_id)', {
+            creator_account_id: accountIds,
+          })
+          .andWhere("d.public_link != ''")
+          .select('creator_account_id', 'creator_account_id')
+          .addSelect('count(1)', 'dataset_count')
+          .addSelect('SUM( d.view_count )', 'total_view_count')
+          .addSelect('SUM( d.share_count ) ', 'total_share_count')
+          .addSelect('SUM( d.fork_count )', 'total_fork_count')
+          .addSelect('SUM( d.favorite_count )', 'total_favorite_count')
+          .groupBy('d.creator_account_id');
+    
+        let statisticRecords = await query.getRawMany();
+        const resp = {}; 
+        for (const stat of statisticRecords) {
+          resp[stat.creator_account_id] = {
+            count: Number(stat.dataset_count) || 0,
+            total_view_count: Number(stat.total_view_count),
+            total_share_count: Number(stat.total_share_count),
+            total_fork_count: Number(stat.total_fork_count),
+            total_favorite_count: Number(stat.total_favorite_count),
+          };;
+        }
+        return resp;
+      }
 }
