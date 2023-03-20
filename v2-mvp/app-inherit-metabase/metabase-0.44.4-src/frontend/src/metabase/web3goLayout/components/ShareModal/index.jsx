68decfc6227562bb3c056ee9c8b8ef4946ab196f
zhangzhi <4742944894@qq.com>
2023-03-20 10:20:55 +0800

    improve share

--- v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/components/ShareModal/index.jsx
+++ v2-mvp/app-inherit-metabase/metabase-0.44.4-src/frontend/src/metabase/web3goLayout/components/ShareModal/index.jsx
@@ -52,40 +52,39 @@ class Component extends React.Component {
             "dashboardId": this.state.record.id,
             "shareChannel": shareChannel
         }).then(d => {
-            if (shareChannel == 'twitter') {
-                const title = `Check out ${this.state.record.name} on Web3go! Here is the link:`;
-                let metaArr = {
-                    "platform": "twitter",
-                    "dashboardId": this.state.record.id,
-                    "metaData": [
-                        { "key": "twitter:card", "value": "summary_large_image" },
-                        { "key": "twitter:site", "value": "https://web3go.xyz" },
-                        { "key": "twitter:url", "value": this.state.publicUrlOfLinkObj.shareLink },
-                        { "key": "twitter:title", "value": title }
-                    ]
-                };
-                LayoutDashboardApi.getShareUrl(metaArr).then(backInterfaceUrl => {
-                    const url = `https://twitter.com/share?text=${title}&url=${backInterfaceUrl}&via=web3go&hashtags=blockChain%2Cweb3go`;
-                    window.open(url);
-                });
-                // let metaParams = metaArr.toString();
-                // console.log("\r\noriginal metaParams:\r\n");
-                // console.log(metaParams);
-                // // 需要encode两次 因为浏览器会自动decode一次，另一次是服务端会decode
-
-                // metaParams = encodeURIComponent(encodeURIComponent(metaParams));
-
-                // let backInterfaceUrl = `https://web3go.xyz/portal/twittershare?meta=${metaParams}`;
-                // let fullUrl = `https://twitter.com/share?text=${title}&url=${backInterfaceUrl}`;
-                // window.open(fullUrl);
-                // // const url = `https://twitter.com/intent/tweet?text=${title}&url=${encodeURIComponent(d.shareLink)}&via=web3go&hashtags=blockChain%2Cweb3go`;
-                // // window.open(url);
-            }
-            if (shareChannel == 'telegram') {
-                const title = `Check out ${this.state.record.name} on Web3go!`;
-                const url = `https://t.me/share/url?url=${encodeURIComponent(d.shareLink)}&text=${title}`;
-                window.open(url);
-            }
+            window.open(d.shareLink);
+            // if (shareChannel == 'twitter') {
+            //     const title = `Check out ${this.state.record.name} on Web3go! Here is the link:`;
+            //     let metaArr = {
+            //         "platform": "twitter",
+            //         "dashboardId": this.state.record.id,
+            //         "metaData": [
+            //             { "key": "twitter:card", "value": "summary_large_image" },
+            //             { "key": "twitter:site", "value": "https://web3go.xyz" },
+            //             { "key": "twitter:url", "value": this.state.publicUrlOfLinkObj.shareLink },
+            //             { "key": "twitter:title", "value": title }
+            //         ]
+            //     };
+            //     LayoutDashboardApi.getShareUrl(metaArr).then(backInterfaceUrl => {
+            //         const url = `https://twitter.com/share?text=${title}&url=${backInterfaceUrl}&via=web3go&hashtags=blockChain%2Cweb3go`;
+            //         window.open(url);
+            //     });
+            // }
+            // if (shareChannel == 'telegram') {
+            //     const title = `Check out ${this.state.record.name} on Web3go! Here is the link:`;
+            //     let metaArr = {
+            //         "platform": "telegram",
+            //         "dashboardId": this.state.record.id,
+            //         "metaData": [
+            //             { "key": "og:url", "value": this.state.publicUrlOfLinkObj.shareLink },
+            //             { "key": "og:title", "value": title }
+            //         ]
+            //     };
+            //     LayoutDashboardApi.getShareUrl(metaArr).then(backInterfaceUrl => {
+            //         const url = `https://twitter.com/share?text=${title}&url=${backInterfaceUrl}&via=web3go&hashtags=blockChain%2Cweb3go`;
+            //         window.open(url);
+            //     });
+            // }
         })
     }
     handleCopy = () => {
